be87c75fb0c148e10495d5f2f54383a7f05f98c9
Matheus Renó Torres <124406908+RennowT@users.noreply.github.com>
2023-04-27 15:47:56 +0000

    🚧

--- JogoDeAdvinhacao/main.cpp
+++ JogoDeAdvinhacao/main.cpp
@@ -13,64 +13,97 @@ int main()
     int numeroAleatorio = 42;
     bool acertou = false;
 
-    system("clear || cls");
-
-    cout << "       **************************************************\n";
-    cout << "       **************** Jogo Da Advinhação **************\n";
-    cout << "       **************************************************\n\n";
-
-    cout << "       Selecione a Dificuldade: Facil [1]  Normal [2]  Difícil [3]\n\n";
-
     do
     {
-        seletor = entradaInteiro();
-    }while(seletor < 1 || seletor > 3);
-    
-    switch (seletor)
-    {
-    case 1:
-        tentativas = 15;
-        break;
 
-    case 2:
-        tentativas = 10;
-        break;
+        system("clear || cls");
 
-    case 3:
-        tentativas = 5;
-        break;
-    
-    default:
-        break;
-    }
+        cout << "\n\n       **************************************************\n";
+        cout << "       **************** Jogo Da Advinhação **************\n";
+        cout << "       **************************************************\n\n";
 
-    system("clear || cls");
+        cout << "       Selecione a Dificuldade: Facil [1]  Normal [2]  Difícil [3]\n\n";
 
-    cout << "       Digite um número de 1 a 100:\n";
-
-    do
-    {
         do
         {
             seletor = entradaInteiro();
-        } while (seletor < 1 || seletor > 100);
-
-        if(seletor == numeroAleatorio)
-            acertou = true;
-        else 
+        }while(seletor < 1 || seletor > 3);
+        
+        switch (seletor)
         {
-            if(seletor > numeroAleatorio)
-                cout << "       O número chutado é MAIOR do que o sorteado\n";
-            if(seletor < numeroAleatorio)
-                cout << "       O número chutado é MENOR do que o sorteado\n";
+        case 1:
+            tentativas = 15;
+            break;
 
-            tentativas--;
+        case 2:
+            tentativas = 10;
+            break;
+
+        case 3:
+            tentativas = 5;
+            break;
+        
+        default:
+            break;
         }
 
-    } while(!jogoTerminou(tentativas, acertou));
+        system("clear || cls");
 
-    system("clear || cls");
-    
+        cout << "\n\n       Digite um número de 1 a 100:\n\n";
+
+        do
+        {
+            do
+            {
+                seletor = entradaInteiro();
+            } while (seletor < 1 || seletor > 100);
+
+            if(seletor == numeroAleatorio)
+                acertou = true;
+            else 
+            {
+                if(seletor > numeroAleatorio)
+                    cout << "       O número chutado é MAIOR do que o sorteado\n\n";
+                if(seletor < numeroAleatorio)
+                    cout << "       O número chutado é MENOR do que o sorteado\n\n";
+
+                tentativas--;
+            }
+
+        }while(!jogoTerminou(tentativas, acertou));
+
+        system("clear || cls");
+
+        if(acertou == true)
+        {
+            cout << "\n\n       Parabens você ganhou!\n\n";
+            cout << "              ___________\n";
+            cout << "             '._==_==_=_.'\n";
+            cout << "             .-\\:      /-.\n";
+            cout << "            | (|:.     |) |\n";
+            cout << "             '-|:.     |-'\n";
+            cout << "               \\::.    /\n";
+            cout << "                '::. .'\n";
+            cout << "                  ) (\n";
+            cout << "                _.' '._\n";
+            cout << "               `\"\"\"\"\"\"\"`\n\n";
+        }
+        else
+        {
+            cout << "\n\n       Você perdeu! Tente na proxima!\n\n";
+        }
+
+        do
+        {
+            cout << "       Jogar Novamente:  Sim [1]  Não [2]\n\n";
+            seletor = entradaInteiro();
+        }while(seletor < 1 || seletor > 2);
+
+        if(seletor == 2)
+            system("clear || cls");
+
+    }while(seletor == 1);
+
     return 0;
 }
 
