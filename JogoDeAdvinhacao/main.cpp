e008d3769df6134210c7483b38c655e30e05e66b
Matheus Renó Torres <124406908+RennowT@users.noreply.github.com>
2023-04-27 03:06:59 +0000

    🚧

--- JogoDeAdvinhacao/main.cpp
+++ JogoDeAdvinhacao/main.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main()
 {
+    int seletor;
+
+    cout << "       **************************************************\n";
+    cout << "       **************** Jogo Da Advinhação **************\n";
+    cout << "       **************************************************\n\n";
+
+    cout << "       Selecione a Dificuldade: Facil [1]  Normal [2]  Difícil [3]\n\n";
+
+    do
+    {
+        cout << ">>> ";
+        cin >> seletor;
+        cout << "\n";
+    }while(seletor < 1 || seletor > 3);
+    
+
+
+
     return 0;
 }
